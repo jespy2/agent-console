f4359a81081e92e2f367eedbdec60189def5e52c
jespy2 <jespy2@gmail.com>
2026-01-28 15:05:37 -0700

    fix ts linting

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tseslint, { parser } from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -11,13 +11,19 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
+      parser: tseslint.parser,
       ecmaVersion: 2020,
+      sourceType: "module",
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        projectService: true,
+      }
     },
   },
 ])
